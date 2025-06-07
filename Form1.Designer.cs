155d91cf86a21b71eec35aa8e41a0c140f7f0ae7
Caique7k <caiquealves7k@gmail.com>
2025-06-07 15:36:35 -0300

    criado formulario e validação dos campos se estão ou não preenchidos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            btnEntrar = new Button();
+            txtUsuario = new TextBox();
+            txtSenha = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(108, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 0;
+            label1.Text = "USUÁRIO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(108, 117);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 15);
+            label2.TabIndex = 1;
+            label2.Text = "SENHA";
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Cursor = Cursors.Hand;
+            btnEntrar.Location = new Point(89, 192);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(75, 23);
+            btnEntrar.TabIndex = 2;
+            btnEntrar.Text = "ENTRAR";
+            btnEntrar.UseVisualStyleBackColor = true;
+            btnEntrar.Click += btnEntrar_Click;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(51, 65);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(158, 23);
+            txtUsuario.TabIndex = 3;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(51, 149);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(158, 23);
+            txtSenha.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(265, 263);
+            Controls.Add(txtSenha);
+            Controls.Add(txtUsuario);
+            Controls.Add(btnEntrar);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button btnEntrar;
+        private TextBox txtUsuario;
+        private TextBox txtSenha;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,21 @@ namespace atividade
         public Form1()
         {
             InitializeComponent();
+            txtSenha.UseSystemPasswordChar = true;
+        }
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text.Trim();
+            string senha = txtSenha.Text.Trim();
+
+            if (string.IsNullOrEmpty(usuario) ||  string.IsNullOrEmpty(senha))
+            {
+                MessageBox.Show("Preencha todos os campos!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                
+            }
         }
     }
 }
