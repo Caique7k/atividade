7de9babe455cfda9359e7e4a1fe0b09ec408fde7
Caique7k <caiquealves7k@gmail.com>
2025-06-08 11:40:12 -0300

    Criado função btnBuscarCep_Click, que utiliza a API viaCep para adicionar o endereço do cliente

--- FormCadastroCliente.Designer.cs
+++ FormCadastroCliente.Designer.cs
@@ -28,12 +28,315 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormCadastroCliente";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            txtboxNOME = new TextBox();
+            txtboxCPF = new TextBox();
+            txtboxEMAIL = new TextBox();
+            txtboxCEP = new TextBox();
+            txtboxLOGRADOURO = new TextBox();
+            txtboxNUMERO = new TextBox();
+            txtboxBAIRRO = new TextBox();
+            txtboxCIDADE = new TextBox();
+            txtboxESTADO = new TextBox();
+            txtboxTELEFONE = new TextBox();
+            txtboxWHATSAPP = new TextBox();
+            label12 = new Label();
+            btnCANCELAR = new Button();
+            btnSALVAR = new Button();
+            btnBuscarCep = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 15);
+            label1.TabIndex = 0;
+            label1.Text = "NOME: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(34, 15);
+            label2.TabIndex = 1;
+            label2.Text = "CPF: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(16, 77);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 15);
+            label3.TabIndex = 2;
+            label3.Text = "EMAIL:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 110);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 15);
+            label4.TabIndex = 3;
+            label4.Text = "ENDEREÇO: ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(16, 137);
+            label5.Name = "label5";
+            label5.Size = new Size(34, 15);
+            label5.TabIndex = 4;
+            label5.Text = "CEP: ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(263, 9);
+            label6.Name = "label6";
+            label6.Size = new Size(67, 15);
+            label6.TabIndex = 5;
+            label6.Text = "TELEFONE: ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(16, 211);
+            label7.Name = "label7";
+            label7.Size = new Size(54, 15);
+            label7.TabIndex = 6;
+            label7.Text = "CIDADE: ";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(12, 172);
+            label8.Name = "label8";
+            label8.Size = new Size(63, 15);
+            label8.TabIndex = 7;
+            label8.Text = "NÚMERO: ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(203, 211);
+            label9.Name = "label9";
+            label9.Size = new Size(55, 15);
+            label9.TabIndex = 8;
+            label9.Text = "ESTADO: ";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(171, 137);
+            label10.Name = "label10";
+            label10.Size = new Size(86, 15);
+            label10.TabIndex = 9;
+            label10.Text = "LOGRADOURO";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(203, 172);
+            label11.Name = "label11";
+            label11.Size = new Size(54, 15);
+            label11.TabIndex = 10;
+            label11.Text = "BAIRRO: ";
+            // 
+            // txtboxNOME
+            // 
+            txtboxNOME.Location = new Point(66, 6);
+            txtboxNOME.Name = "txtboxNOME";
+            txtboxNOME.Size = new Size(134, 23);
+            txtboxNOME.TabIndex = 11;
+            // 
+            // txtboxCPF
+            // 
+            txtboxCPF.Location = new Point(66, 40);
+            txtboxCPF.Name = "txtboxCPF";
+            txtboxCPF.Size = new Size(134, 23);
+            txtboxCPF.TabIndex = 12;
+            // 
+            // txtboxEMAIL
+            // 
+            txtboxEMAIL.Location = new Point(66, 74);
+            txtboxEMAIL.Name = "txtboxEMAIL";
+            txtboxEMAIL.Size = new Size(134, 23);
+            txtboxEMAIL.TabIndex = 13;
+            // 
+            // txtboxCEP
+            // 
+            txtboxCEP.Location = new Point(56, 134);
+            txtboxCEP.Name = "txtboxCEP";
+            txtboxCEP.Size = new Size(100, 23);
+            txtboxCEP.TabIndex = 14;
+            // 
+            // txtboxLOGRADOURO
+            // 
+            txtboxLOGRADOURO.Location = new Point(263, 134);
+            txtboxLOGRADOURO.Name = "txtboxLOGRADOURO";
+            txtboxLOGRADOURO.Size = new Size(100, 23);
+            txtboxLOGRADOURO.TabIndex = 15;
+            // 
+            // txtboxNUMERO
+            // 
+            txtboxNUMERO.Location = new Point(81, 169);
+            txtboxNUMERO.Name = "txtboxNUMERO";
+            txtboxNUMERO.Size = new Size(100, 23);
+            txtboxNUMERO.TabIndex = 16;
+            // 
+            // txtboxBAIRRO
+            // 
+            txtboxBAIRRO.Location = new Point(263, 169);
+            txtboxBAIRRO.Name = "txtboxBAIRRO";
+            txtboxBAIRRO.Size = new Size(100, 23);
+            txtboxBAIRRO.TabIndex = 17;
+            // 
+            // txtboxCIDADE
+            // 
+            txtboxCIDADE.Location = new Point(81, 208);
+            txtboxCIDADE.Name = "txtboxCIDADE";
+            txtboxCIDADE.Size = new Size(100, 23);
+            txtboxCIDADE.TabIndex = 18;
+            // 
+            // txtboxESTADO
+            // 
+            txtboxESTADO.Location = new Point(263, 208);
+            txtboxESTADO.Name = "txtboxESTADO";
+            txtboxESTADO.Size = new Size(100, 23);
+            txtboxESTADO.TabIndex = 19;
+            // 
+            // txtboxTELEFONE
+            // 
+            txtboxTELEFONE.Location = new Point(336, 6);
+            txtboxTELEFONE.Name = "txtboxTELEFONE";
+            txtboxTELEFONE.Size = new Size(100, 23);
+            txtboxTELEFONE.TabIndex = 20;
+            // 
+            // txtboxWHATSAPP
+            // 
+            txtboxWHATSAPP.Location = new Point(336, 35);
+            txtboxWHATSAPP.Name = "txtboxWHATSAPP";
+            txtboxWHATSAPP.Size = new Size(100, 23);
+            txtboxWHATSAPP.TabIndex = 21;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(256, 40);
+            label12.Name = "label12";
+            label12.Size = new Size(74, 15);
+            label12.TabIndex = 22;
+            label12.Text = "WHATSAPP: ";
+            // 
+            // btnCANCELAR
+            // 
+            btnCANCELAR.Cursor = Cursors.Hand;
+            btnCANCELAR.Location = new Point(280, 269);
+            btnCANCELAR.Name = "btnCANCELAR";
+            btnCANCELAR.Size = new Size(75, 23);
+            btnCANCELAR.TabIndex = 23;
+            btnCANCELAR.Text = "CANCELAR";
+            btnCANCELAR.UseVisualStyleBackColor = true;
+            btnCANCELAR.Click += btnCANCELAR_Click;
+            // 
+            // btnSALVAR
+            // 
+            btnSALVAR.Cursor = Cursors.Hand;
+            btnSALVAR.Location = new Point(361, 269);
+            btnSALVAR.Name = "btnSALVAR";
+            btnSALVAR.Size = new Size(75, 23);
+            btnSALVAR.TabIndex = 24;
+            btnSALVAR.Text = "SALVAR";
+            btnSALVAR.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscarCep
+            // 
+            btnBuscarCep.Cursor = Cursors.Hand;
+            btnBuscarCep.Location = new Point(26, 269);
+            btnBuscarCep.Name = "btnBuscarCep";
+            btnBuscarCep.Size = new Size(88, 23);
+            btnBuscarCep.TabIndex = 25;
+            btnBuscarCep.Text = "BUSCAR CEP";
+            btnBuscarCep.UseVisualStyleBackColor = true;
+            btnBuscarCep.Click += btnBuscarCep_Click;
+            // 
+            // FormCadastroCliente
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(463, 304);
+            Controls.Add(btnBuscarCep);
+            Controls.Add(btnSALVAR);
+            Controls.Add(btnCANCELAR);
+            Controls.Add(label12);
+            Controls.Add(txtboxWHATSAPP);
+            Controls.Add(txtboxTELEFONE);
+            Controls.Add(txtboxESTADO);
+            Controls.Add(txtboxCIDADE);
+            Controls.Add(txtboxBAIRRO);
+            Controls.Add(txtboxNUMERO);
+            Controls.Add(txtboxLOGRADOURO);
+            Controls.Add(txtboxCEP);
+            Controls.Add(txtboxEMAIL);
+            Controls.Add(txtboxCPF);
+            Controls.Add(txtboxNOME);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormCadastroCliente";
+            Text = "FormCadastroCliente";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox txtboxNOME;
+        private TextBox txtboxCPF;
+        private TextBox txtboxEMAIL;
+        private TextBox txtboxCEP;
+        private TextBox txtboxLOGRADOURO;
+        private TextBox txtboxNUMERO;
+        private TextBox txtboxBAIRRO;
+        private TextBox txtboxCIDADE;
+        private TextBox txtboxESTADO;
+        private TextBox txtboxTELEFONE;
+        private TextBox txtboxWHATSAPP;
+        private Label label12;
+        private Button btnCANCELAR;
+        private Button btnSALVAR;
+        private Button btnBuscarCep;
     }
 }
--- FormCadastroCliente.cs
+++ FormCadastroCliente.cs
@@ -7,14 +7,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Runtime.ConstrainedExecution;
 
 namespace atividade
 {
     public partial class FormCadastroCliente : Form
     {
+        public class Endereco
+        {
+            public string Cep { get; set; }
+            public string Logradouro { get; set; }
+            public string Complemento { get; set; }
+            public string Bairro { get; set; }
+            public string Localidade { get; set; }
+            public string Uf { get; set; }
+            public string Ibge { get; set; }
+            public string Gia { get; set; }
+            public string Ddd { get; set; }
+            public string Siafi { get; set; }
+        }
+
         public FormCadastroCliente()
         {
             InitializeComponent();
+            txtboxBAIRRO.ReadOnly = true;
+            txtboxCIDADE.ReadOnly = true;
+            txtboxLOGRADOURO.ReadOnly = true;
+            txtboxESTADO.ReadOnly = true;
+
+
+        }
+
+        private void btnCANCELAR_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private async void btnBuscarCep_Click(object sender, EventArgs e)
+        {
+            string cep = txtboxCEP.Text.Trim();
+            if (string.IsNullOrEmpty(cep) || cep.Length != 8)
+            {
+                MessageBox.Show("Por favor, insira um CEP válido com 8 dígitos.");
+                return;
+            }
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = $"https://viacep.com.br/ws/{cep}/json/";
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        Endereco endereco = JsonSerializer.Deserialize<Endereco>(jsonResponse, new JsonSerializerOptions
+                        {
+                            PropertyNameCaseInsensitive = true
+                        });
+
+                        if (endereco != null)
+                        {
+                            txtboxLOGRADOURO.Text = endereco.Logradouro;
+                            txtboxBAIRRO.Text = endereco.Bairro;
+                            txtboxCIDADE.Text = endereco.Localidade;
+                            txtboxESTADO.Text = endereco.Uf;
+
+                            txtboxBAIRRO.ReadOnly = false;
+                            txtboxCIDADE.ReadOnly = false;
+                            txtboxLOGRADOURO.ReadOnly = false;
+                            txtboxESTADO.ReadOnly = false;
+
+                            txtboxNUMERO.Focus();
+                        }
+                        else
+                        {
+                            MessageBox.Show("CEP não encontrado.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erro ao buscar CEP. Verifique o CEP informado.");
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro ao buscar CEP: " + ex.Message);
+            }
         }
     }
 }
